b442748ef7f3faed49c51dccadaccbc667d191a1
M. Mahmudin <42290713+mahmudindev@users.noreply.github.com>
2025-10-01 17:40:02 +0700

    Hotfix for 1.21.4

--- neoforge/src/main/java/com/github/mahmudindev/mcmod/worldportal/neoforge/WorldPortalNeoForge.java
+++ neoforge/src/main/java/com/github/mahmudindev/mcmod/worldportal/neoforge/WorldPortalNeoForge.java
@@ -1,12 +1,13 @@
 package com.github.mahmudindev.mcmod.worldportal.neoforge;
 
 import com.github.mahmudindev.mcmod.worldportal.WorldPortal;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.AddReloadListenerEvent;
+import net.neoforged.neoforge.event.AddServerReloadListenersEvent;
 
 @Mod(WorldPortal.MOD_ID)
 public final class WorldPortalNeoForge {
@@ -18,13 +19,19 @@ public final class WorldPortalNeoForge {
         // Run our common setup.
         WorldPortal.init();
 
-        NeoForge.EVENT_BUS.addListener((AddReloadListenerEvent event) -> {
-            event.addListener(new ResourceManagerReloadListener() {
-                @Override
-                public void onResourceManagerReload(ResourceManager resourceManager) {
-                    WorldPortal.onResourceManagerReload(resourceManager);
-                }
-            });
+        NeoForge.EVENT_BUS.addListener((AddServerReloadListenersEvent event) -> {
+            event.addListener(
+                    ResourceLocation.fromNamespaceAndPath(
+                            WorldPortal.MOD_ID,
+                            "default"
+                    ),
+                    new ResourceManagerReloadListener() {
+                        @Override
+                        public void onResourceManagerReload(ResourceManager resourceManager) {
+                            WorldPortal.onResourceManagerReload(resourceManager);
+                        }
+                    }
+            );
         });
     }
 }
